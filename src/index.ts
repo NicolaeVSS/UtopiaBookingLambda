cea3fbef2aa6a5a59b932c130562405ac59c5343
NicolaeVSS <nicolae.vasile@smoothstack.com>
2019-11-30 17:03:27 -0500

    removed commented code

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import "reflect-metadata";
 import {createConnection} from "typeorm";
 import * as express from "express";
 import * as bodyParser from "body-parser";
-// import * as cors from "cors";
 import {Request, Response} from "express";
 import {Routes} from "./routes";
 
