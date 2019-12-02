807dbfdf6ffa03f7e3570ad695a0802f89c70ff9
NicolaeVSS <nicolae.vasile@smoothstack.com>
2019-12-02 11:56:37 -0500

    health route now just prints I'm Alive!

--- src/index.ts
+++ src/index.ts
@@ -37,7 +37,7 @@ async function bootstrap(){
 
     app.get('/', async (req, res) => {
         console.log("health check\n");
-        return res.status(200).json({message:await connection.query(`SELECT * FROM utopia.airport`) });
+        return res.status(200).json({ message:"I'm alive!" + new Date() });
     });
 
     // register express routes from defined application routes
