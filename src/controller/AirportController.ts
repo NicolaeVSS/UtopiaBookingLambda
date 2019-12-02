b1fafa55cfbb159166afbaa3ecb5e9625d75b777
NicolaeVSS <nicolae.vasile@smoothstack.com>
2019-12-02 11:53:27 -0500

    implicit default db connection

--- src/controller/AirportController.ts
+++ src/controller/AirportController.ts
@@ -4,7 +4,7 @@ import {Airport} from "../entity/Airport";
 
 export class AirportController {
 
-    private airportRepository = getRepository(Airport, "default");
+    private airportRepository = getRepository(Airport);
 
     async all(request: Request, response: Response, next: NextFunction){
         return this.airportRepository.find();
